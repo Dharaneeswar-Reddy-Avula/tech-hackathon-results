913916581e5748fa96b21c78d157d38b729c7ab0
Dhanu <avuladharaneeswarreddy123@gmail.com>
2024-12-16 23:38:38 +0530

    newchanges

--- src/Components/Winners.jsx
+++ src/Components/Winners.jsx
@@ -3,8 +3,8 @@ import { Card } from './card'
 import { Hero } from './Hero'
 export const Winners = () => {
   return (
-    <div className='winners w-full md:p-[50px]'>
-     <div className="winners-cards-wrapper  w-full flex gap-[20px] justify-between items-center pb-[30px] rounded-lg flex-wrap">
+    <div className='winners w-full md:p-[50px] border-none'>
+     <div className="winners-cards-wrapper  w-full flex gap-[20px] justify-between items-center pb-[30px] rounded-lg flex-wrap ">
      <Hero/>
      <Card/>
      </div>
